11c1d8572bc59b14efa8130a2ef19096af1186ce
sharkye1 <84673957+sharkye1@users.noreply.github.com>
2025-12-07 17:20:16 +0300

    v1.1 Full new styles Refactor sources page and added new function "🗺️ Просмотреть на карте (View on map)"

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // Глобальная конфигурация сайта
-export const SITE_VERSION = '1.1.0b';
+export const SITE_VERSION = '1.1';
 export const SITE_NAME = 'Фронтовые Дневники';
 export const SITE_DESCRIPTION = 'Интерактивная карта дневников фронтовиков Великой Отечественной войны';
